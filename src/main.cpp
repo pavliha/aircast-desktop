97aa1ce0d18d26bc463de1223a2596dabd789d5c
Pavlo Kostiuk <kostyk.pavel.09@gmail.com>
2025-02-15 12:23:43 +0400

    remove debug messages

--- src/main.cpp
+++ src/main.cpp
@@ -7,23 +7,11 @@ int main(int argc, char *argv[]) {
   QGuiApplication app(argc, argv);
   QQmlApplicationEngine engine;
 
-  // Debug available resources
-  qDebug() << "Available resources:";
-  QDirIterator it(":", QDirIterator::Subdirectories);
-  while (it.hasNext()) {
-    qDebug() << it.next();
-  }
-
   const QUrl url(QStringLiteral("qrc:/myproject/qml/main.qml"));
-
-  qDebug() << "Import paths:" << engine.importPathList();
-  qDebug() << "Resource URL:" << url;
 
   engine.load(url);
 
   if (engine.rootObjects().isEmpty()) {
-    qDebug() << "Failed to load QML file!";
-    qDebug() << "Engine errors:" << engine.hasError();
     return -1;
   }
 
